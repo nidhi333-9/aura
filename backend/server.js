02b300bdd171a6604287c7a4d1ce03bf439b4f57
Techy Nidhi <nidhi129kumari@gmail.com>
2026-04-04 19:28:06 +0530

    fix cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,12 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://aura-gamma-eight.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Routes
